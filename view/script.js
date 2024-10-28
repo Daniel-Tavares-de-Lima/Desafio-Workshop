ee88be6af2f72f6452cf0e7ab21cda84f94e1523
Daniel Tavares de Lima Marcelino <toys.tokio@gmail.com>
2024-10-28 08:54:16 -0300

    Feita a função para carregar os colaboradores no front

--- view/script.js
+++ view/script.js
@@ -7,6 +7,23 @@ fetch(`${url}/colaboradores`).then(responde => responde.json()).then(data =>{
  })
 
 
-
+// Função para buscar e exibir a lista de colaboradores
+function colaboradores(){
+    fetch(`${url}/colaboradores`).then(response => response.json()).then(colaborador => {
+        const tabelaColaboradores = document.querySelector("#tabelaColaboradores tbody");
+        tabelaColaboradores.innerHTML = "";
+        colaborador.forEach(colaboradorTag =>{
+            const linha = document.createElement("tr");
+            linha.innerHTML = `
+                <td>${colaboradorTag.id}</td>
+                <td>${colaboradorTag.nome}</td>
+            `;
+            tabelaColaboradores.appendChild(linha);
+        });
+    }).catch(error => console.error("Erro ao carregar colaboradores:", error));
+}
  
-  
+// Carregar dados ao carregar as páginas
+document.addEventListener("DOMContentLoaded", () => {
+    if (document.getElementById("tabelaColaboradores")) colaboradores();
+});
